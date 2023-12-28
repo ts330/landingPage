e38f1f929af3abc5c759894f2731415a0adfa372
DarkEaglee <princk1430@gmail.com>
2023-12-28 19:19:39 +0530

    aboutpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo: 'home', pathMatch: 'full' },
+  {path:'home',component:HomepageComponent},
+  {path:'about',component:AboutpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
